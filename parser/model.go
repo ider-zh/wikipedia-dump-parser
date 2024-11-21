2d0a6e346679e993e3e17135b73f548b05ded121
ider <admin@knogen.cn>
2024-11-21 16:17:11 +0800

    export more model

--- parser/model.go
+++ parser/model.go
@@ -1,38 +1,38 @@
 package parser
 
 // json schema 注释
-type text struct {
+type Text struct {
 	Value   string `xml:",chardata" json:"value"`
 	Bytes   int32  `xml:"bytes,attr" json:"bytes"`
 	Deleted string `xml:"deleted,attr" json:"deleted"`
 }
 
-type redirect struct {
+type Redirect struct {
 	Title string `xml:"title,attr" json:"title"`
 }
 
-type contributor struct {
+type Contributor struct {
 	Username string `xml:"username" json:"username"`
 	Ip       string `xml:"ip" json:"ip"`
 	ID       int64  `xml:"id" json:"id"`
 	Deleted  string `xml:"deleted,attr" json:"deleted"`
 }
 
-type revision struct {
+type Revision struct {
 	ID          int64       `xml:"id" json:"id"`
-	Text        text        `xml:"text" json:"text"`
+	Text        Text        `xml:"text" json:"text"`
 	Parentid    int64       `xml:"parentid" json:"parentid"`
 	Timestamp   string      `xml:"timestamp" json:"timestamp"`
 	Comment     string      `xml:"comment" json:"comment"`
 	Model       string      `xml:"model" json:"model"`
 	Format      string      `xml:"format" json:"format"`
-	Contributor contributor `xml:"contributor" json:"contributor"`
+	Contributor Contributor `xml:"contributor" json:"contributor"`
 }
 
 type Page struct {
 	ID        int64      `xml:"id" json:"id"`
 	Ns        int32      `xml:"ns" json:"ns"`
 	Title     string     `xml:"title" json:"title"`
-	Redirect  *redirect  `xml:"redirect" json:"redirect"`
-	Revisions []revision `xml:"revision" json:"revisions"`
+	Redirect  *Redirect  `xml:"redirect" json:"redirect"`
+	Revisions []Revision `xml:"revision" json:"revisions"`
 }
