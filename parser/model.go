fc2f650920cfcf9b3f33eeb3fd87246b4db7bf88
ider <admin@knogen.cn>
2024-11-21 12:38:46 +0800

    add json schema

--- parser/model.go
+++ parser/model.go
@@ -1,37 +1,38 @@
 package parser
 
+// json schema 注释
 type text struct {
-	Value   string `xml:",chardata"`
-	Bytes   int32  `xml:"bytes,attr"`
-	Deleted string `xml:"deleted,attr"`
+	Value   string `xml:",chardata" json:"value"`
+	Bytes   int32  `xml:"bytes,attr" json:"bytes"`
+	Deleted string `xml:"deleted,attr" json:"deleted"`
 }
 
 type redirect struct {
-	Title string `xml:"title,attr"`
+	Title string `xml:"title,attr" json:"title"`
 }
 
 type contributor struct {
-	Username string `xml:"username"`
-	Ip       string `xml:"ip"`
-	ID       int64  `xml:"id"`
-	Deleted  string `xml:"deleted,attr"`
+	Username string `xml:"username" json:"username"`
+	Ip       string `xml:"ip" json:"ip"`
+	ID       int64  `xml:"id" json:"id"`
+	Deleted  string `xml:"deleted,attr" json:"deleted"`
 }
 
 type revision struct {
-	ID          int64       `xml:"id"`
-	Text        text        `xml:"text"`
-	Parentid    int64       `xml:"parentid"`
-	Timestamp   string      `xml:"timestamp"`
-	Comment     string      `xml:"comment"`
-	Model       string      `xml:"model"`
-	Format      string      `xml:"format"`
-	Contributor contributor `xml:"contributor"`
+	ID          int64       `xml:"id" json:"id"`
+	Text        text        `xml:"text" json:"text"`
+	Parentid    int64       `xml:"parentid" json:"parentid"`
+	Timestamp   string      `xml:"timestamp" json:"timestamp"`
+	Comment     string      `xml:"comment" json:"comment"`
+	Model       string      `xml:"model" json:"model"`
+	Format      string      `xml:"format" json:"format"`
+	Contributor contributor `xml:"contributor" json:"contributor"`
 }
 
 type Page struct {
-	ID        int64      `xml:"id"`
-	Ns        int32      `xml:"ns"`
-	Title     string     `xml:"title"`
-	Redirect  *redirect  `xml:"redirect"`
-	Revisions []revision `xml:"revision"`
+	ID        int64      `xml:"id" json:"id"`
+	Ns        int32      `xml:"ns" json:"ns"`
+	Title     string     `xml:"title" json:"title"`
+	Redirect  *redirect  `xml:"redirect" json:"redirect"`
+	Revisions []revision `xml:"revision" json:"revisions"`
 }
